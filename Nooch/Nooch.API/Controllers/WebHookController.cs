ed0d41a3cb5d0dc4602bb113ac92798dd36ef124
malkitsingh <malkitsingh01@gmail.com>
2016-06-04 17:00:52 +0530

    code sync-- working on webhook..not complete yet

--- Nooch/Nooch.API/Controllers/WebHookController.cs
+++ Nooch/Nooch.API/Controllers/WebHookController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json.Linq;
 using Nooch.Common;
+using Nooch.Data;
 
 namespace Nooch.API.Controllers
 {
@@ -17,7 +19,34 @@ namespace Nooch.API.Controllers
 
 
             // code to store transaction timeline info in db 
+            //JObject jsonfromsynapse = JObject.Parse(jsonContent);
+            //JToken token = jsonfromsynapse["jsonContent"];
+            //string transIdFromSyanpse = jsonfromsynapse["trans"]["_id"]["$oid"].ToString();
+            //if (jsonfromsynapse != null)
+            //{
+            //    foreach (JToken jT in jsonfromsynapse)
+            //    {
+            //        string dateFromSynase =
+            //         jT["date"]["$date"].ToString();
 
+            //        string note = jT["note"].ToString();
+            //        string status = jT["status"].ToString();
+            //        string status_id = jT["status_id"].ToString();
+
+            //        string transaction_id_from_synapse = jT["status_id"].ToString();
+
+            //        using (NOOCHEntities obj = new NOOCHEntities())
+            //        {
+            //            TransactionsStatusAtSynapse tas = new TransactionsStatusAtSynapse();
+            //            tas.Nooch_Transaction_Id = transId;
+            //            tas.Transaction_oid
+
+            //            obj.TransactionsStatusAtSynapses
+            //        }
+
+
+            //    }
+            //}
 
 
 
