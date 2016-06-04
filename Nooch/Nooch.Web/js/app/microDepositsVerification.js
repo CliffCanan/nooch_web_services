c28fc9b48d8885d9f19a99892c7c2ef1bca0a074
surya <suryaprakash9@hotmail.com>
2016-06-04 12:03:55 +0530

    added Paesley js validation on Micro Deposit page

--- Nooch/Nooch.Web/js/app/microDepositsVerification.js
+++ Nooch/Nooch.Web/js/app/microDepositsVerification.js
@@ -27,94 +27,104 @@ $(document).ready(function () {
 });
 
 
-
 function SubmitInfo() {
-    console.log('SubmitInfo fired');
-    $("#Submit").text('Submitting...');
-    $('#Submit').attr('disabled', 'disabled');
-  
-    var dot = ".";
-    var IsRs = $('#IsRs').val();
-    var MemberId = $('#MemberId').val().trim();
-    var MicroDepositOne = dot+$('#MicroDepositOne').val();
-    var MicroDepositTwo = dot+ $('#MicroDepositTwo').val();
-    //var MicroDepositOne = $('#MicroDepositOne').val();
-    //var MicroDepositTwo = $('#MicroDepositTwo').val();
-    var bankId = $('#bankId').val();
-    var BankName = $('#BankName').val();
+        
+    if (($('#MicroDepositOne').val().length > 0) && ($('#MicroDepositTwo').val().length > 0))  {
+      
+
+    //if ((MicroDepositOne && MicroDepositTwo) != null) {      
+
+        console.log('SubmitInfo fired');
+        $("#Submit").text('Submitting...');
+        $('#Submit').attr('disabled', 'disabled');
+
+        var dot = ".";
+        var IsRs = $('#IsRs').val();
+        var MemberId = $('#MemberId').val().trim();
+
+        var MicroDepositOne = dot + $('#MicroDepositOne').val();
+        var MicroDepositTwo = dot + $('#MicroDepositTwo').val();
+        var bankId = $('#bankId').val();
+        var BankName = $('#BankName').val();
 
 
-    console.log("SAVE MEMBER INFO -> {IsRs: " + IsRs +
-                                   ", MemberId: " + MemberId +
-                                   ", MicroDepositOne: " + MicroDepositOne +
-                                    ", MicroDepositTwo: " + MicroDepositTwo +
-                                     ", BankName: " + BankName +
-                                   ", bankId " + bankId + "}");
+        console.log("SAVE MEMBER INFO -> {IsRs: " + IsRs +
+                                       ", MemberId: " + MemberId +
+                                       ", MicroDepositOne: " + MicroDepositOne +
+                                        ", MicroDepositTwo: " + MicroDepositTwo +
+                                         ", BankName: " + BankName +
+                                       ", bankId " + bankId + "}");
 
 
-    $.ajax({
-        type: "POST",
-        url: "MFALoginWithRoutingAndAccountNumber",
-        //url: $('#SynapseV3MFABankVerifyWithMicroDepositsUrl').val(),
-        data: "{  'Bank':'" + BankName +
-             "', 'MemberId':'" + MemberId +
-             "', 'MicroDepositOne':'" + MicroDepositOne +
-             "', 'MicroDepositTwo':'" + MicroDepositTwo +
-             "', 'bankId':'" + bankId +
-             "', 'IsRs':'" + IsRs + "'}",
+        $.ajax({
+            type: "POST",
+            url: "MFALoginWithRoutingAndAccountNumber",
+            //url: $('#SynapseV3MFABankVerifyWithMicroDepositsUrl').val(),
+            data: "{  'Bank':'" + BankName +
+                 "', 'MemberId':'" + MemberId +
+                 "', 'MicroDepositOne':'" + MicroDepositOne +
+                 "', 'MicroDepositTwo':'" + MicroDepositTwo +
+                 "', 'bankId':'" + bankId +
+                 "', 'IsRs':'" + IsRs + "'}",
 
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        async: "true",
-        cache: "false",
-        success: function (result) {
-            console.log("SUCCESS -> Save Member Info result is... [next line]");
-            console.log(result);
-            resultReason = result.msg;
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            async: "true",
+            cache: "false",
+            success: function (result) {
+                console.log("SUCCESS -> Save Member Info result is... [next line]");
+                console.log(result);
+                resultReason = result.msg;
 
-            $("#Submit").text('Submit');
-            $('#Submit').removeAttr("disabled");
+                $("#Submit").text('Submit');
+                $('#Submit').removeAttr("disabled");
+                $('#MicroDepositOne').val('');
+                $('#MicroDepositTwo').val('');
 
-            if (result.Is_success == true) {
-                console.log("Success == true");
+                if (result.Is_success == true) {
+                    console.log("Success == true");
 
-                            
-                // THEN DISPLAY SUCCESS ALERT...
-                swal({
-                    title: "Submitted Successfully",
-                    text: "Thanks for submitting your information.",
-                    //"<span>Next, link any checking account to complete this payment:</span>" +
-                    //"<span class=\"spanlist\"><span>1. &nbsp;Select your bank</span><span>2. &nbsp;Login with your regular online banking credentials</span><span>3. &nbsp;Choose which account to use</span></span>",
-                    type: "success",
-                    showCancelButton: false,
+
+                    // THEN DISPLAY SUCCESS ALERT...
+                    swal({
+                        title: "Submitted Successfully",
+                        text: "Thanks for submitting your information.",
+                        //"<span>Next, link any checking account to complete this payment:</span>" +
+                        //"<span class=\"spanlist\"><span>1. &nbsp;Select your bank</span><span>2. &nbsp;Login with your regular online banking credentials</span><span>3. &nbsp;Choose which account to use</span></span>",
+                        type: "success",
+                        showCancelButton: false,
+                        confirmButtonColor: "#3fabe1",
+                        confirmButtonText: "Great!",
+                        closeOnConfirm: true,
+                        html: true,
+                        customClass: "idVerSuccessAlert",
+                    });
+                }
+                    //else if (result.mfaMessage.indexOf('Incorrect microdeposit amounts') != -1)
+                    // swal({
+                    //     title: "Unexpected Error",
+                    //     text: result.mfaMessage,
+                    //     type: "error",
+                    //     confirmButtonColor: "#3fabe1",
+                    //     confirmButtonText: "Ok",
+                    //     html: true
+                    // });
+
+                else swal({
+                    title: "Unexpected Error",
+                    text: "Something went wrong - extremely sorry about this. We hate it when something breaks!",
+                    type: "error",
                     confirmButtonColor: "#3fabe1",
-                    confirmButtonText: "Great!",
-                    closeOnConfirm: true,
-                    html: true,
-                    customClass: "idVerSuccessAlert",
-                });              
+                    confirmButtonText: "Ok",
+                    html: true
+                });
+
             }
-           //else if (result.mfaMessage.indexOf('Incorrect microdeposit amounts') != -1)
-           // swal({
-           //     title: "Unexpected Error",
-           //     text: result.mfaMessage,
-           //     type: "error",
-           //     confirmButtonColor: "#3fabe1",
-           //     confirmButtonText: "Ok",
-           //     html: true
-           // });
-
-        else swal({
-            title: "Unexpected Error",
-            text: "Something went wrong - extremely sorry about this. We hate it when something breaks!",
-            type: "error",
-            confirmButtonColor: "#3fabe1",
-            confirmButtonText: "Ok",
-            html: true
-        });
-
         }
+        )
     }
-    )}
 
-           
+    else {
+        return false;
+    }
+}
