43eeaa47522616d238f7f689ab41c230e0ce3137
Cliff Canan <cliff@nooch.com>
2016-06-06 19:25:04 -0400

    • Merging

--- Nooch/Nooch.DataAccess/MembersDataAccess.cs
+++ Nooch/Nooch.DataAccess/MembersDataAccess.cs
@@ -2627,7 +2627,7 @@ namespace Nooch.DataAccess
                 memberObj.SSN = !String.IsNullOrEmpty(ssn) ? CommonHelper.GetEncryptedData(ssn) : null;
                 memberObj.DateOfBirth = dateofbirth;
                 memberObj.Status = "Active";
-                memberObj.UDID1 =   !String.IsNullOrEmpty(fngprnt) ? fngprnt : memberObj.UDID1;
+                memberObj.UDID1 = !String.IsNullOrEmpty(fngprnt) ? fngprnt : memberObj.UDID1; // if this has changed since what it originally was, then the user will eventually have to complete the 2FA for Synapse
                 memberObj.DateModified = DateTime.Now;
                 memberObj.cipTag = !String.IsNullOrEmpty(cip) ? cip : memberObj.cipTag;
                 memberObj.FacebookUserId = !String.IsNullOrEmpty(fbid) ? fbid : memberObj.FacebookUserId;
